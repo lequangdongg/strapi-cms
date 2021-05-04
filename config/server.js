4d06a1ca5c051d9c5601a83873de9dfebc7d67b9
Dong Le <lequangdongg@gmail.com>
2021-05-04 20:32:12 +0700

    initial project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '508bc712f23bac48690be0af70aa9c41'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '489ae235-9179-472e-a41f-ef04603e7256'
+};
